15a7538fc4d20c85bfed0b7ad3a244684367cf1d
Terry D'Arcy <terrykdarcy@gmail.com>
2021-01-21 17:43:37 +0000

    mobile updates

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -14,12 +14,15 @@ function SpashScreen() {
 
   return (
     <div className="splash_screen">
-      <div className="splash_card">
-        <div className="splash_title">
-          <img width="70px" src={money} alt="cash group" />
-          <h1>Cash Group</h1>
+      <div className="splash_card_top"></div>
+      <div className="splash_card_bottom">
+        <div className="splash_card">
+          <div className="splash_title">
+            <img width="70px" src={money} alt="cash group" />
+            <h1>Cash Group</h1>
+          </div>
+          <div className="splash_content">{Logintoggled ? <CreateAccount /> : <Login />}</div>
         </div>
-        <div className="splash_content">{Logintoggled ? <CreateAccount /> : <Login />}</div>
       </div>
     </div>
   );
