99d7b978bedd05e1ca1105a1080b6957818d0a77
Terry D'Arcy <terrykdarcy@gmail.com>
2021-01-21 16:37:40 +0000

    updated icon

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -17,7 +17,7 @@ function SpashScreen() {
       <div className="splash_card">
         <div className="splash_title">
           <img width="70px" src={money} alt="cash group" />
-          <h1>Cash Group github test 1</h1>
+          <h1>Cash Group</h1>
         </div>
         <div className="splash_content">{Logintoggled ? <CreateAccount /> : <Login />}</div>
       </div>
